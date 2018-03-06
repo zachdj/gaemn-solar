ca386622a2269dad4c25c1cd1de7094bb67b7c46
Zach Jones <zachydj@gmail.com>
2018-03-06 13:33:53 -0500

    Modify RF experiment to run over all sites and prediction hours

--- experiments/rf.py
+++ experiments/rf.py
@@ -6,14 +6,21 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from texttable import Texttable
+import os
 
 from solarrad import Dataset
 
-# generate a dataset
-dataset = Dataset.generate_from_query(target_hour=24, site_id=115, gaemn=True, window=True, nam_cell=True, nam_grid=False)
+# site ids for the five sites in Sam's paper
+sites = {
+    "griffin": 115,
+    "jonesboro": 380,
+    "attapulgus": 190,
+    "blairsville": 150,
+    "brunswick": 420
+}
 
 # params for rf regressor
-params = {
+rf_params = {
     "n_estimators": 20,
     "criterion": "mse",
     "max_features": None,
@@ -23,15 +30,40 @@ params = {
     "random_state": 123
 }
 
-X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.labels, test_size=0.20, random_state=123)
 
-regressor = RandomForestRegressor(**params)
-regressor.fit(X_train, y_train)
+def do_experiment(hour, site, gaemn=False, window=False, nam_cell=False, nam_grid=False):
+    dataset = Dataset.generate_from_query(target_hour=hour, site_id=site, gaemn=gaemn, window=window, nam_cell=nam_cell,
+                                          nam_grid=nam_grid)
+    X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.labels, test_size=0.20, random_state=123)
+    regressor = RandomForestRegressor(**rf_params)
+    regressor.fit(X_train, y_train)
+    predictions = regressor.predict(X_test)
+    mae = mean_absolute_error(y_test, predictions)
+    return mae
 
-predictions = regressor.predict(X_test)
-correlation = r2_score(y_test, predictions)
-mse = mean_squared_error(y_test, predictions)
-mae = mean_absolute_error(y_test, predictions)
 
-print("RF on %s" % dataset.name)
-print("Corr: %0.4f \n MSE: %0.4f \n MAE: %0.4f" % (correlation, mse, mae))
+# iterate over all sites
+for site_name, site_id in sites.items():
+    print("Working on site %s" % site_name)
+    # construct TextTable for writing out results
+    table = Texttable()
+    table.set_cols_align(["c", "c", "c", "c", "c", "c"])
+    table.header(["Hour", "gaemn only", "gaemn+window", "gaemn + window + single-cell", "gaemn + window + multicell", "% decrease"])
+
+    # do an experiment for each hour and each dataset
+    for hour in list(range(1, 37)):
+        print("Hour %d" % hour)
+        all_attr_mae = do_experiment(hour, site_id, gaemn=True)
+        window_mae = do_experiment(hour, site_id, gaemn=True, window=True)
+        singlecell_mae = do_experiment(hour, site_id, gaemn=True, window=True, nam_cell=True)
+        multicell_mae = do_experiment(hour, site_id, gaemn=True, window=True, nam_cell=True, nam_grid=True)
+        pct_improvement = (window_mae - multicell_mae) * 100 / window_mae
+        table.add_row([hour, all_attr_mae, window_mae, singlecell_mae, multicell_mae, pct_improvement])
+
+    # output the table to a file:
+    outpath = os.path.join("../results", site_name+".txt")
+    with open(outpath, 'w') as outfile:
+        print(site_name.capitalize(), file=outfile)
+        print(table.draw(), file=outfile)
+
+print("Done!")
