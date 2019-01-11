4eeb3ff190159a49ea6b3d4a0fc18c8433801c99
Zach Jones <zachydj@gmail.com>
2019-01-11 11:38:04 -0500

    Refactor Dataset API

--- solardata/Dataset.py
+++ solardata/Dataset.py
@@ -4,85 +4,81 @@ import numpy as np
 import pandas as pd
 import mysql.connector
 
-from solardata.query import build as build_query
+from solardata.query import Query
 import solardata.config as config
 
 
-def generate_from_csv(filepath, name=None):
-    """ Reads a dataset from a csv file at the given location
-
-    :param filepath: the location of the csv file on the local filesystem
-    :param name: (Optional) logical name for the dataset.  Defaults to the filepath
-    :return: Dataset object representing the contents of the csv file
-    """
-    df = pd.read_csv(filepath, sep=',')
-    X = df[df.columns.tolist()[:-1]].values
-    y = df[df.columns.tolist()[-1]].values
-    column_names = list(df.axes[1])
-
-    if name is None:
-        name = filepath
-
-    return Dataset(name, X, y, attr_labels=column_names)
-
-
-def generate_from_query(target_hour=1, site_id=115, gaemn=True, window=False, nam_cell=False, nam_grid=False,
-                        start_date='2011-06-22', end_date='2012-04-30 23:45:00', name=None):
-
-    X = []  # attributes
-    y = []  # target
-
-    # establish connection to mySQL database
-    db_config = config.to_dict()
-    cnx = mysql.connector.connect(**db_config)
-    cursor = cnx.cursor()
-
-    # build and execute the query
-    query_string = build_query(target_hour, site_id, gaemn, window, nam_cell, nam_grid, start_date, end_date)
-
-    cursor.execute(query_string)
-    attribute_names = [i[0] for i in cursor.description]
-    for record in cursor:
-        X.append(
-            np.array(record[:-1]).astype(float)
-        )
-        y.append(float(record[-1]))
-    cnx.close()
-
-    if name is None:
-        name = f'data_{target_hour}hour'
-        if gaemn: name += '_gaemn'
-        if window: name += '_window'
-        if nam_cell: name += '_cell'
-        if window: name += '_multi'
-
-    return Dataset(name, X, y, attr_labels=attribute_names)
-
-
 class Dataset(object):
-    def __init__(self, name, X, y=None, attr_labels=None):
-        """
-        Object-oriented interface representing a dataset
-        X is an array of examples, where each example has n attributes
-        y is an (optional) array of labels or values
+    def __init__(self, name=None, query=None, csv_file=None):
+        """ Represents a dataset of (example, label) pairs
 
-        :param name: a logical name for this dataset
-        :param X: an array of solarrad points without labels
-        :param y: the labels corresponding to X
-        :param attr_labels: labels for the columns of X and y
+        Datasets can be initialized using a SQL query, which will be executed against the database specified by the
+        `config.db.json` file.
+
+        Datasets can be initialized by passing the full path to a csv file via the `csv_file` argument.
+
+        If both the `query` and `csv_file` arguments are provided, then the dataset will be initialized using the
+        SQL query and ignore the csv file.
+
+        If neither of the `query` or `csv_file` arguments are provided, the dataset will be empty.
+
+        Args:
+            name (str): logical name for the dataset
+            query (Query or None): if set, intialize the dataset by executing the provided solardata.query.Query
+            csv_file (str or None): if set, initialize the dataset using the csv file at this location
         """
+        name = name
+        examples = []
+        labels = []
+        column_names = []
+
+        if query is not None:
+            # establish connection to mySQL database
+            db_config = config.to_dict()
+            cnx = mysql.connector.connect(**db_config)
+
+            # execute query
+            cursor = cnx.cursor()
+            cursor.execute(str(query))
+            column_names = [i[0] for i in cursor.description]
+            for record in cursor:
+                examples.append(
+                    np.array(record[:-1]).astype(float)
+                )
+                labels.append(float(record[-1]))
+            cnx.close()
+
+            if name is None:
+                name = f'data_{query.target_hour}hour'
+                if query.components['gaemn']: name += '_gaemn'
+                if query.components['window']: name += '_window'
+                if query.components['nam_cell']: name += '_nam'
+                if query.components['nam_grid']: name += '_grid'
+
+        elif csv_file is not None:
+            dataframe = pd.read_csv(csv_file, sep=',')
+            examples = dataframe[dataframe.columns.tolist()[:-1]].values
+            labels = dataframe[dataframe.columns.tolist()[-1]].values
+            column_names = list(dataframe.axes[1])
+
+            if name is None:
+                name = csv_file
+
         self.name = str(name)
-        self.data = np.array(X)
-        self.labels = np.array(y)
-        self.attr_labels = attr_labels
+        self.examples = np.array(examples)
+        self.labels = np.array(labels)
+        self.column_names = column_names
 
-    def write_to_file(self, directory, filename=None):
-        """
-        Writes the dataset as a csv file to <filename>.csv in the given directory
-        If filename is None, the the Dataset name will be used as the filename
-        :param directory: the directory to write this dataset
-        :param filename: the filename that this dataset will be written to
-        :return: the full path of the output file
+    def dump_to_csv(self, directory, filename=None):
+        """ Dumps the dataset to a csv file
+
+        Args:
+            directory (str): location where the dataset will be written
+            filename (str or None): if set, the output file will be named <filename>.csv
+
+        Returns:
+            str: path to the output file
+
         """
         if filename is None:
             filename = self.name + ".csv"
@@ -93,7 +89,7 @@ class Dataset(object):
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        # assemble solarrad into csv form
+        # reshape data to be tabular
         labels = np.reshape(self.labels, (len(self.labels), 1))
         data = np.append(self.data, labels, axis=1)
 
@@ -101,8 +97,8 @@ class Dataset(object):
         with open(filepath, 'w') as outfile:
             writer = csv.writer(outfile, delimiter=",")
             # write a header row if the attribute names are known
-            if self.attr_labels is not None:
-                writer.writerow(self.attr_labels)
+            if len(self.column_names) > 0:
+                writer.writerow(self.column_names)
 
             # write a line for each example
             for example in data:
